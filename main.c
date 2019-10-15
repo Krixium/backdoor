feec41f4c7734595f4934a8939a2d578a013a75c
William Murphy <will.murphy1994@gmail.com>
2019-10-15 15:08:37 -0700

    Changes: - Added CMake function for finding libpcap. - Modified .gitignore to whitelist FindPCAP.cmake and ignore VS Code workspace files. - Added function declaration for mask_process()

--- main.c
+++ main.c
@@ -1,4 +1,20 @@
+#include <pcap.h>
+#include <stdio.h>
+
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/prctl.h>
+
+int mask_process(const char* original, const char* mask);
+
 int main(int argc, char *argv[])
 {
+    char* errbuf[PCAP_ERRBUF_SIZE];
+    printf("Hello World!!!\n");
     return 0;
+}
+
+int mask_process(const char* original, const char* mask) {
+    
 }
