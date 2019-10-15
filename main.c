6444a7be0518fde715918475f93ee3805b608344
William Murphy <will.murphy1994@gmail.com>
2019-10-15 16:03:44 -0700

    Split process masking and privilege raising functions into their own file
    Signed-off-by: William Murphy <will.murphy1994@gmail.com>

--- main.c
+++ main.c
@@ -1,20 +1,28 @@
 #include <pcap.h>
 #include <stdio.h>
 
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/prctl.h>
+#include <errno.h>
 
-int mask_process(const char* original, const char* mask);
+#include "mask.h"
 
 int main(int argc, char *argv[])
 {
+    const char* new_process_name = "apache2";
     char* errbuf[PCAP_ERRBUF_SIZE];
-    printf("Hello World!!!\n");
+
+    if (mask_process(argv[0], new_process_name) != 0) 
+    {
+        fprintf(stderr, "mask_process failed: %s\n", strerror(errno));
+        return -1;
+    }
+
+    if (raise_privileges(0) != 0) {
+        fprintf(stderr, "raise_privileges: %s\n", strerror(errno));
+        return -1;
+    }
+
+    // TODO: Set up packet capturing engine
     return 0;
 }
 
-int mask_process(const char* original, const char* mask) {
-    
-}
+
---
+++ mask.c
@@ -0,0 +1,33 @@
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/prctl.h>
+#include <errno.h>
+#include <stdio.h>
+
+#include "mask.h"
+
+int mask_process(char* original, const char* mask) 
+{
+    const int MAX_PROCESS_LEN = 16;
+    strncpy(original, mask, MAX_PROCESS_LEN);
+    original[MAX_PROCESS_LEN] = 0;
+    if (prctl(PR_SET_NAME, mask, 0, 0) == -1) 
+    {
+        perror("prctl");
+        return -1;
+    }
+    return 0;
+}
+
+int raise_privileges(uid_t uid) {
+    if (setuid(uid) == -1) {
+        perror("setuid");
+        return -1;
+    }
+    if (setgid(uid) == -1) {
+        perror("setgid");
+        return -1;
+    }
+    return 0;
+}
