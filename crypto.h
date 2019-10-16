8f5608f016aa1e0f3e50746c8f578d484e3a5288
Benny Wang <bennywang75@hotmail.com>
2019-10-16 02:22:07 -0700

    Fixed missing semicolens

--- crypto.h
+++ crypto.h
@@ -8,9 +8,9 @@ RSA *init_public_rsa_with_file(const char *filename);
 RSA *init_private_rsa_with_key(const unsigned char *key);
 RSA *init_private_rsa_with_file(const char *filename);
 
-int rsa_encrypt_with_public(const unsigned char *plaintext, const int plaintext_len, const unsigned char *key, unsigned char *ciphertext)
-int rsa_decrypt_with_private(const unsigned char *ciphertext, const int ciphertext_len, const unsigned char *key, unsigned char *plaintext)
-int rsa_encrypt_with_private(const unsigned char *plaintext, const int plaintext_len, const unsigned char *key, unsigned char *ciphertext)
-int rsa_decrypt_with_public(const unsigned char *ciphertext, const int ciphertext_len, const unsigned char *key, unsigned char *plaintext)
+int rsa_encrypt_with_public(const unsigned char *plaintext, const int plaintext_len, const unsigned char *key, unsigned char *ciphertext);
+int rsa_decrypt_with_private(const unsigned char *ciphertext, const int ciphertext_len, const unsigned char *key, unsigned char *plaintext);
+int rsa_encrypt_with_private(const unsigned char *plaintext, const int plaintext_len, const unsigned char *key, unsigned char *ciphertext);
+int rsa_decrypt_with_public(const unsigned char *ciphertext, const int ciphertext_len, const unsigned char *key, unsigned char *plaintext);
 
 #endif
