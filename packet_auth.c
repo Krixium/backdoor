bdca3f65591370adad38b3c7cad0a90b08116717
Benny Wang <bennywang75@hotmail.com>
2019-10-16 20:18:06 -0700

    Added temp reference code

--- packet_auth.c
+++ packet_auth.c
@@ -4,8 +4,3 @@ int is_packet_authenticated(const unsigned char *packet, const int packet_len)
 {
     return 0;
 }
-
-void authenticate_packet(unsigned char *packet, const int packet_len)
-{
-
-}
--- packet_auth.h
+++ packet_auth.h
@@ -2,6 +2,5 @@
 #define PACKET_AUTH_H
 
 int is_packet_authenticated(const unsigned char *packet, const int packet_len);
-void authenticate_packet(unsigned char *packet, const int packet_len);
 
 #endif
