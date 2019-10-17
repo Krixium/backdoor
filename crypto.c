778b54c3593ac607b18ecfbba157f4bf53620abd
Benny Wang <bennywang75@hotmail.com>
2019-10-16 20:56:29 -0700

    Changed the encryption scheme from RSA to XOR

--- crypto.c
+++ crypto.c
@@ -1,124 +1,43 @@
 #include "crypto.h"
 
-#include <openssl/bio.h>
-#include <openssl/pem.h>
-
-static const int DEFAULT_PADDING_MODE = RSA_PKCS1_PADDING;
-
 /*
-* Creates a RSA key structure from the given public key in PEM format.
-*
-* Params:
-*       const unsigned char *key: The public key in PEM format.
-*
-* Returns:
-*       THe RSA key structure created from the public key PEM.
-*/
-RSA *create_public_rsa_key(const unsigned char *key)
+ * Performs XOR encryption.
+ *
+ * Params:
+ *      const char *key: The key to use for encryption.
+ *      const int key_len: The length of key.
+ *      const char *plaintext: The plaintext to encrypt.
+ *      const int plaintext_len: The length of plaintext.
+ *      char *ciphertext: A pointer to the output buffer. Must be the same size as plaintext.
+ *
+ * Returns:
+ *      1 if the encryption was successful, 0 otherwise.
+ */
+void xor_encrypt(const char *key, const int key_len, const char* plaintext, const int plaintext_len, char *ciphertext)
 {
-    RSA *rsa = NULL;
-    BIO *keybio = BIO_new_mem_buf(key, -1);
-
-    if (keybio == NULL)
+    for (int i = 0; i < plaintext_len; i++)
     {
-        return 0;
+        ciphertext[i] = plaintext[i] ^ key[i % key_len];
     }
-
-    rsa = PEM_read_bio_RSA_PUBKEY(keybio, &rsa, NULL, NULL);
-    return rsa;
 }
 
 /*
-* Creates a RSA key structure from the given public key in PEM format.
-*
-* Params:
-*       const unsigned char *key: The private key in PEM format.
-*
-* Returns:
-*       THe RSA key structure created from the private key PEM.
-*/
-RSA *create_private_rsa_key(const unsigned char *key)
+ * Performs xor decryption.
+ *
+ * Params:
+ *      const char *key: The key to use for encryption.
+ *      const int key_len: The length of key.
+ *      const char *ciphertext: The ciphertext to decrypt.
+ *      const int ciphertext_len: The length of the ciphertext.
+ *      char *plaintext: A pointer to the output buffer. Must be the same size as ciphertext.
+ *
+ * Returns:
+ *      1 if the decryption was successful, 0 otherwise.
+ */
+void xor_decrypt(const char *key, const int key_len, const char* ciphertext, const int ciphertext_len, char *plaintext)
 {
-    RSA *rsa = NULL;
-    BIO *keybio = BIO_new_mem_buf(key, -1);
-
-    if (keybio == NULL)
+    for (int i = 0; i < ciphertext_len; i++)
     {
-        return 0;
+        plaintext[i] = ciphertext[i] ^ key[i % key_len];
     }
-
-    rsa = PEM_read_bio_RSAPrivateKey(keybio, &rsa, NULL, NULL);
-    return rsa;
-}
-
-/*
-* Encrypts data with the RSA public key.
-*
-* Params:
-*       const unsigned char *plaintext: Pointer to the block of plaintext.
-*       const int plaintext_len: Length of the plaintext.
-*       const unsigned char *key: Pointer to the public key in PEM format.
-*       unsigned char *ciphertext: A pointer to a buffer to hold ciphertext.
-*
-* Returns:
-*       The length of the ciphertext.
-*/
-int rsa_encrypt_with_public(const unsigned char *plaintext, const int plaintext_len, const unsigned char *key, unsigned char *ciphertext)
-{
-    RSA *rsa = create_public_rsa_key(key);
-    return RSA_public_encrypt(plaintext_len, plaintext, ciphertext, rsa, DEFAULT_PADDING_MODE);
-}
-
-/*
-* Decrypts data with the RSA private key.
-*
-* Params:
-*       const unsigned char *ciphertext: Pointer to the block of ciphertext.
-*       const int ciphertext_len: Length of the ciphertext.
-*       const unsigned char *key: Pointer to the private key in PEM format.
-*       unsigned char *plaintext: A pointer to a buffer to hold the plaintext.
-*
-* Returns:
-*       The length of the plaintext.
-*/
-int rsa_decrypt_with_private(const unsigned char *ciphertext, const int ciphertext_len, const unsigned char *key, unsigned char *plaintext)
-{
-    RSA *rsa = create_private_rsa_key(key);
-    return RSA_private_decrypt(ciphertext_len, ciphertext, plaintext, rsa, DEFAULT_PADDING_MODE);
-}
-
-/*
-* Encrypts data with the RSA private key.
-*
-* Params:
-*       const unsigned char *plaintext: Pointer to the block of plaintext.
-*       const int plaintext_len: Length of the plaintext.
-*       const unsigned char *key: Pointer to the private key in PEM format.
-*       unsigned char *ciphertext: A pointer to a buffer to hold ciphertext.
-*
-* Returns:
-*       The length of the ciphertext.
-*/
-int rsa_encrypt_with_private(const unsigned char *plaintext, const int plaintext_len, const unsigned char *key, unsigned char *ciphertext)
-{
-    RSA *rsa = create_private_rsa_key(key);
-    return RSA_private_encrypt(plaintext_len, plaintext, ciphertext, rsa, DEFAULT_PADDING_MODE);
-}
-
-/*
-* Decrypts data with the RSA public key.
-*
-* Params:
-*       const unsigned char *ciphertext: Pointer to the block of ciphertext.
-*       const int ciphertext_len: Length of the ciphertext.
-*       const unsigned char *key: Pointer to the public key in PEM format.
-*       unsigned char *plaintext: A pointer to a buffer to hold the plaintext.
-*
-* Returns:
-*       The length of the plaintext.
-*/
-int rsa_decrypt_with_public(const unsigned char *ciphertext, const int ciphertext_len, const unsigned char *key, unsigned char *plaintext)
-{
-    RSA *rsa = create_public_rsa_key(key);
-    return RSA_public_decrypt(ciphertext_len, ciphertext, plaintext, rsa, DEFAULT_PADDING_MODE);
 }
--- crypto.h
+++ crypto.h
@@ -1,16 +1,7 @@
 #ifndef CRYPTO_H
 #define CRYPTO_H
 
-#include <openssl/rsa.h>
-
-RSA *init_public_rsa_with_key(const unsigned char *key);
-RSA *init_public_rsa_with_file(const char *filename);
-RSA *init_private_rsa_with_key(const unsigned char *key);
-RSA *init_private_rsa_with_file(const char *filename);
-
-int rsa_encrypt_with_public(const unsigned char *plaintext, const int plaintext_len, const unsigned char *key, unsigned char *ciphertext);
-int rsa_decrypt_with_private(const unsigned char *ciphertext, const int ciphertext_len, const unsigned char *key, unsigned char *plaintext);
-int rsa_encrypt_with_private(const unsigned char *plaintext, const int plaintext_len, const unsigned char *key, unsigned char *ciphertext);
-int rsa_decrypt_with_public(const unsigned char *ciphertext, const int ciphertext_len, const unsigned char *key, unsigned char *plaintext);
+void xor_encrypt(const char *key, const int key_len, const char* plaintext, const int plaintext_len, char *ciphertext);
+void xor_decrypt(const char *key, const int key_len, const char* ciphertext, const int ciphertext_len, char *plaintext);
 
 #endif
